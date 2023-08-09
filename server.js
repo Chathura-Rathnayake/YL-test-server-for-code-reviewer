ad6669377b6e224fb2653cd9f6920bfff16cc4c6
Chathura <chathurar@yaalalabs.com>
2023-08-09 10:21:52 +0530

    commit test - 2

--- server.js
+++ server.js
@@ -6,20 +6,16 @@ const port = process.env.PORT || 3000;
 //make an http request to a public api - https://api.coindesk.com/v1/bpi/currentprice.json
 
 async function get_bitcoin_prices() {
-  await axios({
+  let resp = await axios({
     method: "get",
     url: "https://api.coindesk.com/v1/bpi/currentprice.json",
-  }).then(
-    (response) => {
-      console.log(response);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  });
+
+  return resp;
 }
 
-get_bitcoin_prices();
+let xx = await get_bitcoin_prices();
+console.log(xx);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;

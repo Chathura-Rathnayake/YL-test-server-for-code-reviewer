bca6050706202825ba4f057ac3f6e8eb945e618d
Chathura <chathurar@yaalalabs.com>
2023-08-09 10:03:34 +0530

    Adding new API changes - 1

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const http = require('http');
+const http = require("http");
+const axios = require("axios");
 
 const port = process.env.PORT || 3000;
 
+//make an http request to a public api - https://api.coindesk.com/v1/bpi/currentprice.json
+
+async function get_bitcoin_prices() {
+  await axios({
+    method: "get",
+    url: "https://api.coindesk.com/v1/bpi/currentprice.json",
+  }).then(
+    (response) => {
+      console.log(response);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+get_bitcoin_prices();
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
 });
 
 server.listen(port, () => {
